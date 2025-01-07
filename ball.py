ea2941784bf2b7dc7179d5aed78fe7eea861a3a2
carnasci <cen.master24@gmail.com>
2025-01-07 13:58:17 -0600

    Day 22 - ball increases speed a little bit every time it contacts a paddle, Day-22 finished

--- ball.py
+++ ball.py
@@ -9,6 +9,7 @@ class Ball(Turtle):
         self.penup()
         self.x_move = 10
         self.y_move = 10
+        self.move_speed = 0.1
 
 
     def move(self):
@@ -21,7 +22,9 @@ class Ball(Turtle):
 
     def paddle_bounce(self):
         self.x_move *= -1
+        self.move_speed *= 0.8
 
     def restart(self):
         self.goto(0, 0)
+        self.move_speed = 0.1
         self.paddle_bounce()
--- main.py
+++ main.py
@@ -18,14 +18,14 @@ scoreboard = Scoreboard()
 
 
 screen.listen()
-screen.onkey(r_paddle.go_up, "Up")
-screen.onkey(r_paddle.go_down, "Down")
-screen.onkey(l_paddle.go_up, "w")
-screen.onkey(l_paddle.go_down, "s")
+screen.onkeypress(r_paddle.go_up, "Up")
+screen.onkeypress(r_paddle.go_down, "Down")
+screen.onkeypress(l_paddle.go_up, "w")
+screen.onkeypress(l_paddle.go_down, "s")
 
 game_is_on = True
 while game_is_on:
-    time.sleep(0.1)
+    time.sleep(ball.move_speed)
     screen.update()
     ball.move()
 
